366fa36163106b4bc6847b2ece7f133da21dcc6c
Moonkey_ <69099850+Moonkey233@users.noreply.github.com>
2022-10-19 16:14:08 +0800

    Delete Minecraft_Map_Painting_Generator directory

--- Minecraft_Map_Painting_Generator/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "mainwindow.h"
-
-int main(int argc, char *argv[]){
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.setWindowTitle("Minecraft Map Painting Generator designed by Moonkey_ 2022");
-    w.show();
-    return a.exec();
-}
